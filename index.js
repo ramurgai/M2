f183d4f4b604881b9ab7d60bec1f3ea022a4a91f
Rahul Murgai <ramurgai@my.waketech.edu>
2025-01-14 13:13:21 -0500

    Gotta push some new stuff

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ document.getElementById("myH1").innerHTML = "I love Javascript (not really)"
 document.getElementById("myParagraph").textContent = "Hi guys"
 
 //Prompts
-const student = prompt("What is your name?: ")
+const student = prompt("What is your name? ")
 console.log(student)
 document.write(`<p>Hello, ${student}!`)
 const messageElement = document.createElement("p");
-messageElement.textContent = `Hello ${student}`;
+messageElement.textContent = `Hello, ${student}`;
 document.body.appendChild(messageElement)
 
 
@@ -27,3 +27,34 @@ document.body.appendChild(messageElement)
 document.body.style.color = "green"
 document.body.style.backgroundColor = "black"
 document.body.style.fontsize = "2em"
+
+// Event Listeners
+document.getElementById("myH1").addEventListener("click", function(){
+    // details what will happen when we click the header
+    document.getElementById("myH1").style.color = "red"
+    alert(`NO diddy, stop touching me ${student}!`)
+})
+
+document.getElementById("myH1").addEventListener("mouseover", function(){
+    document.getElementById("myH1").style.color = "black"
+})
+
+document.getElementById("myH1").addEventListener("dbclick", function(){
+    document.getElementById("myH1").style.fontsize = "6em"
+})
+
+document.getElementById("myParagraph").addEventListener("mouseover", function(){
+    document.getElementById("myParagraph").style.color = "white"
+})
+
+document.getElementById("myParagraph").addEventListener("click", function(){
+    document.getElementById("myParagraph").style.color = "green"
+})
+
+document.getElementById("lightButton").addEventListener("click", function(){
+    document.body.style.backgroundColor = "white"
+})
+
+document.getElementById("darkButton").addEventListener("click", function(){
+    document.body.style.backgroundColor = "black"
+})
