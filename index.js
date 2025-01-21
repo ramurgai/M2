cf1f656630115b2e593cf47ff90c6cb7e7661933
Rahul Murgai <ramurgai@my.waketech.edu>
2025-01-21 12:10:41 -0500

    Getting another site up

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ document.body.style.fontsize = "2em"
 document.getElementById("myH1").addEventListener("click", function(){
     // details what will happen when we click the header
     document.getElementById("myH1").style.color = "red"
-    alert(`NO diddy, stop touching me ${student}!`)
+    alert(`Stop being silly ${student}!`)
 })
 
 document.getElementById("myH1").addEventListener("mouseover", function(){
-    document.getElementById("myH1").style.color = "black"
+    document.getElementById("myH1").style.color = "Green"
 })
 
 document.getElementById("myH1").addEventListener("dbclick", function(){
@@ -48,13 +48,67 @@ document.getElementById("myParagraph").addEventListener("mouseover", function(){
 })
 
 document.getElementById("myParagraph").addEventListener("click", function(){
-    document.getElementById("myParagraph").style.color = "green"
+    document.getElementById("myParagraph").style.color = "black"
 })
 
 document.getElementById("lightButton").addEventListener("click", function(){
     document.body.style.backgroundColor = "white"
+    document.getElementById("myH1").style.color = "black"
+    document.body.style.color = "black"
+
 })
 
 document.getElementById("darkButton").addEventListener("click", function(){
     document.body.style.backgroundColor = "black"
+    document.getElementById("myH1").style.color = "white"
+    document.body.style.color = "white"
+
 })
+
+//Variable Time!
+
+let x; // declaring the variable without initializing
+x = "hello";
+console.log(x);
+const y = 10 ;//declares and initializes and you can't change the value
+let z = 3;
+
+x = 4;
+console.log(x);
+
+z = true;
+console.log(z);
+
+let nombre = "VP Biruk";
+console.log(nombre);
+window.alert(`Hello your name is ${nombre}`);
+
+let stuff = window.prompt("What is the best song ever? ");
+document.write(`<p>The best song ever according to ${student} is ${stuff}`)
+
+let online = false;
+console.log(online)
+console.log(`Online status ${online}`)
+
+//Arithmetic operators
+// +, -, /, *, **, %, ++, --
+
+let students = 24;
+console.log(students)
+students += 1;
+console.log(students)
+
+
+//Data types
+
+let classname = "WEB115";
+console.log(classname)
+console.log(typeof classname)
+
+let pizza = "pizza";
+pizza = Number(pizza)
+console.log(pizza)
+
+
+
+
